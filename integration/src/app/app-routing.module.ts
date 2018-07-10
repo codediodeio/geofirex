47ef96ebe941d530043e9aa1a5ba9c2c9e742634
codediodeio <hello@codediode.io>
2018-07-10 10:18:31 -0700

    spec: add integration tests + demo

--- integration/src/app/app-routing.module.ts
+++ integration/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BasicGeoqueryComponent } from './basic-geoquery/basic-geoquery.component';
+import { RealtimeGeoqueryComponent } from './realtime-geoquery/realtime-geoquery.component';
 
-const routes: Routes = [{ path: '', component: BasicGeoqueryComponent }];
+const routes: Routes = [
+  { path: '', component: BasicGeoqueryComponent },
+  { path: 'rt', component: RealtimeGeoqueryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
