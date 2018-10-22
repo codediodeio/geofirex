7a44ceac690ad9f84667d12e851b1ab2b8d191e9
Jamie Neubert Pedersen <eikooc@users.noreply.github.com>
2018-10-22 10:16:15 +0200

    Update main.spec.js

--- spec/main.spec.ts
+++ spec/main.spec.ts
@@ -65,7 +65,7 @@ describe('RxGeofire', () => {
   });
 
   describe('CollectionRef', () => {
-    let ref: GeoFireCollectionRef;
+    let ref: GeoFireCollectionRef<any>;
     let hash;
     let phx;
     beforeEach(() => {
